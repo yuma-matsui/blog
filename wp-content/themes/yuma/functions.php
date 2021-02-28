6af791f1eab772d685bb43b41aeca914065599f5
yuma-matsui <oshino.iiyo@gmail.com>
2021-02-28 17:00:06 +0900

    カスタム投稿タイプ
    register_post_type

--- wp-content/themes/yuma/functions.php
+++ wp-content/themes/yuma/functions.php
@@ -1,9 +1,19 @@
-<?php 
-function init_func(){
+<?php
+function init_func()
+{
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
 
-  register_post_type('item', )
+  register_post_type('item', [
+    'labels' => [
+      'name' => '商品',
+    ],
+    'public' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-cart'
+  ]);
 }
 
 add_action('init', 'init_func');
